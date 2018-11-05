9b067aeedac6a26547820adb0f1688abc8d728af
“weant <weantvip@163.com>
2018-11-05 22:35:45 +0800

    Initial commit

---
+++ otn_tool/src/main/java/com/otn/tool/Main.java
@@ -0,0 +1,11 @@
+/**
+ * FileName: Main
+ * Author:   Administrator
+ * Date:     2018/11/5 22:34
+ * Description: Main
+ */
+package com.otn.tool;
+
+public class Main {
+
+}
---
+++ ptn_tool/src/main/java/com/ptn/tool/Main.java
@@ -0,0 +1,11 @@
+/**
+ * FileName: Main
+ * Author:   Administrator
+ * Date:     2018/11/5 22:33
+ * Description: Main
+ */
+package com.ptn.tool;
+
+public class Main {
+
+}
